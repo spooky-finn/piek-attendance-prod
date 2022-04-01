3fd78edba283fed9a5d21a0587f759172a6d3349
Ivan Loseev <loseev5@gmail.com>
2022-04-01 20:03:06 +0300

    Revert "Create text.js"
    This reverts commit 6995d706c6e5d8b12a232066ff47c0aff1c000b8.

--- text.js
+++
@@ -1,5 +0,0 @@
-import { execSync } from 'child_process';
-
-const stdout = execSync('mdb-export C:\Users\Миша\Documents acc_monitor_log > ./events.csv', { encoding: 'utf-8'})
-
-console.log('stdout: ', stdout);

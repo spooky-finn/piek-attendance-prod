c45ee7985b5bff808748223c9dddc4d1427b82b3
Ivan Loseev <loseev5@gmail.com>
2022-04-02 00:59:10 +0300

    Create piek_attendance.config.js

---
+++ piek_attendance.config.js
@@ -0,0 +1,20 @@
+import 'dotenv/config'
+
+export const config = {
+    apps: [
+        {
+            name : 'piek_attendance',
+            script: 'index.js',
+            env: {
+                NODE_ENV: process.env.NODE_ENV,
+                HASURA_SECRET: process.env.HASURA_SECRET,
+                GRAPHQL_ENDPOINT: process.env.GRAPHQL_ENDPOINT,
+                WORKSHIFT: process.env.WORKSHIFT,
+                HASURA_DATE_FORMAT: process.env.HASURA_DATE_FORMAT,
+                ZK_SYSTEM_FATE_FORMAT: process.env.ZK_SYSTEM_FATE_FORMAT,
+                INCOMING_DATABASE_NAME: process.env.INCOMING_DATABASE_NAME,
+                SYNC_INTERVAL: process.env.SYNC_INTERVAL,
+            }
+        }
+    ]
+}
